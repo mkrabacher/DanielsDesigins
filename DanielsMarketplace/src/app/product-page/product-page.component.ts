eb35091692e603428be439c5f3d7f80f593baa5c
Matt Krabacher <matt.krabacher@gmail.com>
2018-07-26 17:55:14 -0700

    began rearrangeing marketplace

--- DanielsMarketplace/src/app/product-page/product-page.component.ts
+++ DanielsMarketplace/src/app/product-page/product-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+    selector: 'app-product-page',
+    templateUrl: './product-page.component.html',
+    styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+    adminPrivileges: boolean;
 
-  constructor() { }
+    constructor(private _httpService: HttpService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.retrieveCurrentUserLevel();
+    }
 
+    retrieveCurrentUserLevel() {
+        this.adminPrivileges = this._httpService.retrieveCurrentUserLevelInService();
+    }
+
+    addToCart(item, $event) {
+        // gets desired quanitity from number selector
+        const quantity = $event.path[1].childNodes[0].valueAsNumber;
+        this._httpService.addItemToCartInService(item, quantity);
+
+        // unhides 'added' note
+        $event.path[1].children[2].hidden = false;
+    }
+
+    deleteItem(item) {
+        console.log('sending delete request');
+        const observable = this._httpService.deleteItemInService(item);
+        observable.subscribe(data => {
+            console.log('got back: ', data);
+            // no need to get all items anymore
+            // this.getAllItemsThroughService();
+        });
+    }
 }
